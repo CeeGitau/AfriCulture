24ab1fc996fa5b53b496bb67b48a606318adc0b5
CG <96789431+CeeGitau@users.noreply.github.com>
2025-04-17 11:19:40 +0300

    Added Navbar to landing page

--- front/src/pages/landing.js
+++ front/src/pages/landing.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 import "../assets/css/landing.css";
-import { Link } from "react-router-dom";
 
 const Landing = () => {
     return (
         <div>
+            <Navbar />
             <h1>Welcome to AfriCulture 🌍✨</h1>
             <p>Explore and share the beauty of African culture.</p>
         </div>
