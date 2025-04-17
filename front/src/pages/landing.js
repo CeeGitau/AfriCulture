37ee77b900d2b3973b9691e65bead82c46b51aba
CG <96789431+CeeGitau@users.noreply.github.com>
2025-04-17 10:38:47 +0300

    Created landing page

---
+++ front/src/pages/landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../assets/css/landing.css";
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+    return (
+        <div>
+            <h1>Welcome to AfriCulture 🌍✨</h1>
+            <p>Explore and share the beauty of African culture.</p>
+        </div>
+    );
+};
+
+export default Landing;
