36767a4a2a8567fa71d37a2bead0bc98cf8cd86d
CG <96789431+CeeGitau@users.noreply.github.com>
2025-04-17 10:57:35 +0300

    Create UserContext and Navbar

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Landing from "./pages/landing";
+import UserContext from "./contexts/UserContext";
 
 const router = createBrowserRouter([
   { path: '/', element: <Landing /> },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  const [user, setUser] = useState(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <RouterProvider router={router} />
+    </UserContext.Provider>
+  );
 }
 
 export default App;
