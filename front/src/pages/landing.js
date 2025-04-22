3fb41e24bfdc490d0c0e4d4801684877831fbae8
CG <96789431+CeeGitau@users.noreply.github.com>
2025-04-22 12:02:41 +0300

    Added background image

--- front/src/pages/landing.js
+++ front/src/pages/landing.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Logo from "../assets/images/unity.png";
 import "../assets/css/landing.css";
 
 const Landing = () => {
     return (
         <div>
             <Navbar />
-            <h1>Welcome to AfriCulture 🌍✨</h1>
-            <p>Explore and share the beauty of African culture.</p>
+            <div className="landing-container">
+                <img src={Logo} alt="logo" className="landing-logo" />
+                <h1 className="landing-title">Welcome to AfriCulture 🌍✨</h1>
+                <p className="landing-description">Explore and share the beauty of African culture.</p>
+            </div>
         </div>
     );
 };
