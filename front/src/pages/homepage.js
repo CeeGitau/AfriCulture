5d6be22dfd991530533314623b09be903ab5bc72
CG <96789431+CeeGitau@users.noreply.github.com>
2025-04-24 10:48:28 +0300

    Added categories to homepage

--- front/src/pages/homepage.js
+++ front/src/pages/homepage.js
@@ -1,12 +1,44 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
 import "../assets/css/homepage.css";
+
+const categories = [
+    "Food & Cuisine",
+    "Clothing & Fashion",
+    "Music & Dance",
+    "Languages & Culture",
+    "Festivals & Holidays",
+    "Arts & Handicrafts",
+    "Literature & Poetry",
+    "Customs & Traditions",
+    "Religious Practices & Beliefs",
+    "Sports & Games",
+    "Architecture & Design",
+    "Films & Theatre",
+    "Etiquette & Social Norms",
+];
 
 const Homepage = () => {
     return (
         <div>
             <Navbar />
-            <p>Homepage coming soon...</p>
+            <div className="homepage-container">
+                <h2 className="homepage-heading">Explore African Culture</h2>
+                <p className="homepage-subtext">Select a category to dive into its rich heritage:</p>
+                <ul className="categories-list">
+                    {categories.map((category, index) => (
+                        <li key={index} className="category-item">
+                            <Link
+                                to={`/category/${encodeURIComponent(category)}`}
+                                className="category-link"
+                            >
+                                {category}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 };
