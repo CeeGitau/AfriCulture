a21e387bb2ccca8f970379515294b98628fe073b
CG <96789431+CeeGitau@users.noreply.github.com>
2025-05-06 10:51:26 +0300

    Added icons to categories

--- front/src/pages/homepage.js
+++ front/src/pages/homepage.js
@@ -19,6 +19,22 @@ const categories = [
     "Etiquette & Social Norms",
 ];
 
+const categoryIcons = {
+    "Food & Cuisine": require("../assets/images/food and cuisine.png"),
+    "Clothing & Fashion": require("../assets/images/clothing and fashion.png"),
+    "Music & Dance": require("../assets/images/music and dance.png"),
+    "Languages & Culture": require("../assets/images/language and culture.png"),
+    "Festivals & Holidays": require("../assets/images/festivals and holidays.png"),
+    "Arts & Handicrafts": require("../assets/images/arts and handicrafts.png"),
+    "Literature & Poetry": require("../assets/images/literature and poetry.png"),
+    "Customs & Traditions": require("../assets/images/customs and traditions.png"),
+    "Religious Practices & Beliefs": require("../assets/images/religious practices and beliefs.png"),
+    "Sports & Games": require("../assets/images/sports and games.png"),
+    "Architecture & Design": require("../assets/images/architecture and design.png"),
+    "Films & Theatre": require("../assets/images/film and theatre.png"),
+    "Etiquette & Social Norms": require("../assets/images/etiquette and social norms.png"),
+};
+
 const Homepage = () => {
     return (
         <div>
@@ -38,6 +54,7 @@ const Homepage = () => {
                                 to={`/category/${encodeURIComponent(category)}`}
                                 className="category-link"
                             >
+                                <img src={categoryIcons[category]} alt={category} className="category-icon" />
                                 {category}
                             </Link>
                         </li>
