66ab769f91069aa5186c16d92d316756584853a4
CG <96789431+CeeGitau@users.noreply.github.com>
2025-06-16 15:07:46 +0300

    Add backend logic for viewing single post

--- back/controllers/postController.js
+++ back/controllers/postController.js
@@ -48,8 +48,21 @@ const getAllPosts = async (req, res) => {
     }
 };
 
+const getSinglePost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id).populate("user", "username");
+        if (!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+        res.json(post);
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+};
+
 module.exports = {
     CreatePost,
     getPostsByCategory, 
-    getAllPosts
+    getAllPosts,
+    getSinglePost
 };
--- back/routes/postRoutes.js
+++ back/routes/postRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router =express.Router();
 const authMiddleware = require('../middleware/verifyToken');
-const { CreatePost, getPostsByCategory, getAllPosts } = require('../controllers/postController');
+const { CreatePost, getPostsByCategory, getAllPosts, getSinglePost } = require('../controllers/postController');
 
 router.post('/', authMiddleware, CreatePost);
 
 router.get('/category/:category', getPostsByCategory);
 router.get('/all-posts', getAllPosts);
+router.get('/:id', getSinglePost);
 
 module.exports = router;
 
