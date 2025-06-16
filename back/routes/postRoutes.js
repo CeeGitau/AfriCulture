1a197cb5c72d4bf25eaa711cdff022b42d22d169
CG <96789431+CeeGitau@users.noreply.github.com>
2025-06-16 16:21:45 +0300

    Added backend logic to fetch user's posts

--- back/routes/postRoutes.js
+++ back/routes/postRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router =express.Router();
 const authMiddleware = require('../middleware/verifyToken');
-const { CreatePost, getPostsByCategory, getAllPosts, getSinglePost } = require('../controllers/postController');
+const { CreatePost, getPostsByCategory, getAllPosts, getSinglePost, getPostsByUser } = require('../controllers/postController');
 
 router.post('/', authMiddleware, CreatePost);
 
 router.get('/category/:category', getPostsByCategory);
 router.get('/all-posts', getAllPosts);
 router.get('/:id', getSinglePost);
+router.get('/user/:userId', getPostsByUser);
 
 module.exports = router;
 
