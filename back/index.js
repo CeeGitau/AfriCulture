02831e33d8e23b86fe59ac060f75b6a24cc68273
CG <96789431+CeeGitau@users.noreply.github.com>
2026-01-12 09:37:02 +0300

    Added serverless entrypoint (to aid hosting on Vercel)

--- back/index.js
+++ back/index.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
+
 const userRoutes = require('./routes/userRoutes');
 const postRoutes = require('./routes/postRoutes');
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -23,11 +23,21 @@ app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 
 // Sample route
-app.get('/', (req, res) => {
-    res.send('Welcome to AfriConnect backend!');
-});
+// app.get('/', (req, res) => {
+//     res.send('Welcome to AfriConnect backend!');
+// });
 
-// DB connection and server start
-mongoose.connect(process.env.MONGO_URI)
-        .then(() => {app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));})
-        .catch(err => console.log(err));
+// Connect DB ONCE 
+let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) return;
+
+  await mongoose.connect(process.env.MONGO_URI);
+  isConnected = true;
+  console.log("MongoDB connected");
+};
+
+connectDB();
+
+module.exports = app;
