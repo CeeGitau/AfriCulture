a9f8d5535793dce81d11868226491983cefe0ea5
CG <96789431+CeeGitau@users.noreply.github.com>
2026-01-23 11:01:12 +0300

    minor fix

--- back/index.js
+++ back/index.js
@@ -15,8 +15,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Serve the uploads folder 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// ---- LOCAL DEV ONLY ----
+if (process.env.NODE_ENV !== "production") {
+  const path = require('path');
+  app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+}
 
 // Routes
 app.use('/api/users', userRoutes);
